5e710708e5e3f308e29b77f7bcc11474cca71869
farhan2525 <rajveerfarhan959@gmail.com>
2021-06-25 22:22:46 +0600

    full update

--- js/custom.js
+++ js/custom.js
@@ -11,17 +11,19 @@ $('.banner_slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: false
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots:true,
+          arrows:false,
         }
       },
       {
@@ -88,28 +90,26 @@ $('.banner_slider').slick({
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 991,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          slidesToScroll: 2,
+          infinite: false,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
         }
       },
       {
         breakpoint: 576,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-          dots:false,
-          autoplay:true,
+          slidesToScroll: 3,
         }
       }
     ]
@@ -123,19 +123,18 @@ $('.banner_slider').slick({
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 991,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 3,
+          slidesToScroll: 1,
         }
       },
       {
@@ -161,19 +160,19 @@ $('.banner_slider').slick({
     nextArrow:'<i class="fas fa-angle-right next"></i>',
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 991,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
         }
       },
       {
--- js/storyP.js
+++ js/storyP.js
@@ -9,19 +9,21 @@ $('.banner_slider').slick({
     nextArrow:'<i class="fas fa-angle-right next"></i>',
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 991,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          arrows:true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows:false,
+          dots:true
         }
       },
       {
@@ -48,26 +50,26 @@ $('.banner_slider').slick({
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 991,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          slidesToScroll: 2,
+          infinite: false,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
         }
       },
       {
         breakpoint: 576,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToScroll: 3,
         }
       }
     ]
